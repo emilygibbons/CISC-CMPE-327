9324befcf74192baea7a56a1147023aa4f727537
Adam Hirani <hiraniadam@gmail.com>
2020-11-17 13:57:46 -0500

    updated test cases

--- qa327/backend.py
+++ qa327/backend.py
@@ -42,7 +42,8 @@ def register_user(email, name, password, password2, balance):
     """
     hashed_pw = generate_password_hash(password, method='sha256')
     # store the encrypted password rather than the plain password
-    new_user = User(email=email, name=name, password=hashed_pw, balance=balance)
+    new_user = User(email=email, name=name,
+                    password=hashed_pw, balance=balance)
 
     db.session.add(new_user)
     db.session.commit()
@@ -65,61 +66,9 @@ def get_all_tickets():
     """
 
     tickets = Ticket.query.all()
-    
+
     return tickets
 
-def checkEmailFormat(email):
-    """
-    :param email: users entered email
-
-    Take the email are run it through a regex to see if it meets specifications.
-    If it does then return true, if it doesnt then return false.
-    """
-
-    if re.match("\A(?P<name>[\w\-_]+)@(?P<domain>[\w\-_]+).(?P<toplevel>[\w]+)\Z", email, re.IGNORECASE):
-        return True
-    else:
-        return False
-
-
-def checkPasswordFormat(password):
-    """
-    :param password: users entered password
-
-    Take the password are run it through a regex to see if it meets specifications.
-    If it does then return true, if it doesnt then return false.
-    """
-
-    reg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$"
-
-    # compiling regex
-
-    pat = re.compile(reg)
-
-    # searching regex
-
-    mat = re.search(pat, password)
-
-    # validating conditions
-
-    if mat:
-        return True
-    else:
-        return False
-
-
-def checkUserNameFormat(u):
-    """
-    :param u: users entered username
-
-    Take the user are run it through a regex to see if it meets specifications.
-    Also checks that the username does not start or end with a space
-    If it does then return true, if it doesnt then return false.
-    """
-    if bool(re.match('^[a-zA-Z0-9]+$', u)) and (2 < len(u) < 20) and not u.startswith(" ") and not u.endswith(" "):
-        return True
-    else:
-        return False
 
 def sell_ticket(quantity, name, email, price, expiration_date):
     """
@@ -133,9 +82,10 @@ def sell_ticket(quantity, name, email, price, expiration_date):
     of available tickets in the data base.
     """
     for i in range(int(quantity)):
-         new_ticket = Ticket(name=name,email=email, price=price, date=expiration_date)
-         db.session.add(new_ticket)
-         db.session.commit()
+        new_ticket = Ticket(name=name, email=email,
+                            price=price, date=expiration_date)
+        db.session.add(new_ticket)
+        db.session.commit()
     return True
 
 
@@ -147,11 +97,11 @@ def buy_ticket(name, quantity):
     Takes the information inputed in the form on the frontend and,for now, deletes quantity amount
     of tickets from the list of available tickets.
     """
-    
+
     for i in range(int(quantity)):
-         ticket = Ticket.query.filter_by(name=name).first()
-         db.session.delete(ticket)
-         db.session.commit()
+        ticket = Ticket.query.filter_by(name=name).first()
+        db.session.delete(ticket)
+        db.session.commit()
     return True
 
 
@@ -167,9 +117,10 @@ def delete_ticket(quantity, name, price, expiration, email):
     Uses a for loop to delete in range quantity if multiple tickets exist, because quantity is not stored as
     in a Ticket object.
     """
-    
+
     for i in range(int(quantity)):
-         ticket = Ticket.query.filter_by(name=name, price=price, date=expiration, email=email).first()
-         db.session.delete(ticket)
-         db.session.commit()
+        ticket = Ticket.query.filter_by(
+            name=name, price=price, date=expiration, email=email).first()
+        db.session.delete(ticket)
+        db.session.commit()
     return True
